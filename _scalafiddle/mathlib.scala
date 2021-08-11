de9798f33a1e63615545000c2a0f39786149669a
Mark Blokpoel <mark.blokpoel@gmail.com>
2021-08-11 16:42:05 +0200

    Plotly wrappers.

--- _scalafiddle/mathlib.scala
+++ _scalafiddle/mathlib.scala
@@ -494,6 +494,78 @@ case object Plotly {
 
   var plotCounter = 0
 
+  case object PlotType extends Enumeration {
+    type PlotType = Value
+    val Line, Bar, Scatter = Value
+  }
+
+  case class Trace(data: List[(Double, Double)], name: String, plotType: PlotType.PlotType = PlotType.Scatter) {
+    val markers = if(plotType == PlotType.Scatter) "\"mode\": \"markers\"," else ""
+
+    def mean: Trace = {
+      def calcMean(xs: Iterable[Double]) = xs.sum / xs.size
+
+      val meanData = data.toMap.groupBy(_._1)
+        .mapValues(xs => calcMean(xs.map(_._2)))
+        .toList
+
+      Trace(meanData, name, plotType)
+    }
+
+    def toJSON: String = {
+      s"""
+      {
+        "name": "$name",
+        "type": "${plotType.toString.toLowerCase}",
+        $markers
+        "x": ${data.sortBy(_._1).map(_._1).mkString("[",",","]")},
+        "y": ${data.sortBy(_._1).map(_._2).mkString("[",",","]")}
+      }
+      """
+    }
+  }
+
+  case class Plot(traces: List[Trace], xAxisTitle: String = "", yAxisTitle: String = "") {
+    val xAxis = if(!xAxisTitle.isEmpty)
+      s"""
+      ,"xaxis": {
+        "title": {
+          "text": "$xAxisTitle"
+        }
+      }
+      """
+    else ""
+    val yAxis = if(!yAxisTitle.isEmpty)
+      s"""
+      ,"yaxis": {
+        "title": {
+          "text": "$yAxisTitle"
+        }
+      }
+      """
+    else ""
+
+    def toJSON: String = {
+      s"""
+      {
+        "data": ${traces.map(_.toJSON).mkString("[",",","]")},
+        "layout": {
+          "showlegend": "true",
+          "legend": {
+            "x": 1,
+            "xanchor": "right",
+            "y": 1
+          }
+          $xAxis
+          $yAxis
+        }
+      }
+      """
+    }
+
+    def render: Unit = Plotly.render(this.toJSON)
+  }
+
   def render(plotJson: String): Unit = {
     Fiddle.print(
 	  div(id:=s"plot$plotCounter"),
@@ -520,6 +592,9 @@ case object Plotly {
     plotCounter = plotCounter + 1
   }
 }
+
+import Plotly._
+
 val result = {
 ////
 }
