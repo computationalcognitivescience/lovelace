a95fa45c08868944088adc7a56c21f8f6245d151
markblokpoel <mark.blokpoel@gmail.com>
2022-09-14 11:48:22 +0200

    clean up

--- merge_scala.bat
+++
@@ -1,9 +0,0 @@
-del _scalafiddle\mathlib.scala
-
-type _scalafiddle\settheory.scala > _scalafiddle\mathlib.scala
-echo: >> _scalafiddle\mathlib.scala
-
-for /f "delims=" %%a in ('dir /b /s /a-d _scalafiddle\mathlib\*.scala') do type "%%a" >> _scalafiddle\mathlib.scala & echo: >> _scalafiddle\mathlib.scala
-
-type _scalafiddle\main.scala >> _scalafiddle\mathlib.scala
-
